dd6950e987644715f15da69ece810729ba5d67cb
horix77 <horix77777@gmail.com>
2020-06-25 20:17:36 +0200

    modified

--- src/documents/refund.jsx
+++ src/documents/refund.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 let refundInfo = () => {
     return (
         <div className="info1212">
-     <h4>Return & Refund Policy</h4>
-<h5>Refund Policy for "Fortuneauction360.com"</h5>
-<h6>Dated of March 27, 2020</h6>
-<h6>Last updated on May 20, 2020</h6>
-<br/>
-<br/>
+            <h4>Return & Refund Policy</h4>
+            <h5>Refund Policy for "Fortuneauction360.com"</h5>
+            <h6>Dated of March 27, 2020</h6>
+            <h6>Last updated on May 20, 2020</h6>
+            <br />
+            <br />
 
 This Refund Policy (hereinafter - the "Refund Policy") is the annexe to the Agreement
 (hereinafter - the "Agreement") and an official document of the operator (hereinafter - the
@@ -16,51 +16,51 @@ This Refund Policy (hereinafter - the "Refund Policy") is the annexe to the Agre
 procedure for refunds on request of individuals and entities (hereinafter - the "Customer") who
 are interested in purchasing or using the Electronic Services provided by the Operator on the
 website "FORTUNEAUCTION360.COM" (hereinafter - the "Website").
-<br/>
+            <br />
 
 1. All Customers of "FORTUNEAUCTION360.COM" does have the advantage of the
 money-back guarantee; if our services were not genuine. However, you can file your
-complaint to the ​
+complaint to the
 fortuneaction360@gmail.com​ or ​ contact@fortuneauction360.com​ . This
 is because we are in the gambling business and every information needed for you to win
 is provided to you.
-<br/>
+            <br />
 
 2. Refunds will be made to the Customer who:
-<br/>
+            <br />
 
 ● Whom we have violated the terms and conditions; such as not displaying on the
 website to them of the closure of an auction and they bid by paying for it;
-<br/>
+            <br />
 
 ● We mistakenly deducted fund beyond the money charged for a bid price.
-<br/>
+            <br />
 
 ● Who experienced a breach into their momo account as a result of our technical
 fault. After the investigation provides us liable.
-<br/>
+            <br />
 
 ● Whom has bidded and got the fortune numbers and yet our bid target is not
 reached after the deadline set for each product.
-<br/>
+            <br />
 
-3. Refunds will take up to 3 days maximum to be completed.
-<br/>
+3. Refunds will take up to 14 days maximum to be completed.
+            <br />
 
 4. The Operator of the Website has the right to make changes to this Refund Policy. When
 changes are made the Operator notifies the Customers by posting a new edition of the
 Refund Policy. The previous edition of the Refund Policy become no longer valid.
-<br/>
+            <br />
 
 5. Winners have the right to refund items that are not within the required healthy standard or
 condition within 5 days after receiving the item or services. Contact us via
 fortuneaction360@gmail.com​ or ​ contact@fortuneauction360.com​ . If you have any
 question or complaint.
-<br/>
+            <br />
 
-<h6>THIS SITE IS A PILOT PHASE TO CLEARLY UNDERSTAND MARKET DEMAND FOR SUCH A
-PLATFORM. ONLY THOSE AT THE AGE OF 18 IS ALLOWED TO BID. ALSO, BEFORE BIDDING
-CONSIDER YOUR LOCAL LAWS & REGULATION ON RAFFLE BIDDING.
+            <h6>THIS SITE IS A PILOT PHASE TO CLEARLY UNDERSTAND MARKET DEMAND FOR SUCH A
+            PLATFORM. ONLY THOSE AT THE AGE OF 18 IS ALLOWED TO BID. ALSO, BEFORE BIDDING
+            CONSIDER YOUR LOCAL LAWS & REGULATION ON RAFFLE BIDDING.
 </h6>
 
 
